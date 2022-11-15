9a6e825b00c00d2a5899e2c221f4b37a12bd3c06
Optimus <swapnil.shrungare123@gmail.com>
2022-11-15 22:01:23 +0530

    Added dada Definition

---
+++ class/20221115_SESSION_026/data_definition_statement.c
@@ -0,0 +1,73 @@
+/* 
+    Initialized versions of data definitoin statements 
+    of all fundamental data types defined by C 
+*/
+
+char c1 = 'M';          /*  1 byte storage */
+signed char c2 = -109;  /*  1 byte storage */
+
+unsigned char c3 = 215; /* 1 byte storage */
+
+short int s1 = -500;            /* 2 byte storage */
+unsigned short int s2 = 17321;  /* 2 byte storage */
+
+int n1 = 563452;                /*  4 byte storage */
+unsigned int n2 = 365256;       /*  4 byte storage */
+
+long int big_num_1 = 3634635;           /*  4 byte storage */
+unsigned long int big_num_2 = 74543245; /*  4 byte storage */
+
+long long int very_big_num_1 = -36525633466;            /* 8 byte storage*/
+unsigned long long int very_big_num_2 = 36345363265;    /* 8 byte storage*/
+
+float f_num_1 = 5677.432;           /* 4 byte storage */
+double d_num_1 = -365345.4635636;   /* 8 byte storage */
+
+/*  
+    Uninitialized version of data definition statements of 
+    fundamental data types provided by the C programming language
+*/
+
+char c4; 
+signed char c5; 
+unsigned char c6; 
+
+short int s3; 
+unsigned short int s4; 
+
+int n3; 
+unsigned int n4; 
+
+long int lng_n1; 
+unsigned long int u_lng_n1; 
+
+long long int lng_n2; 
+unsigned long long int u_lng_n2; 
+
+/* 
+    Variable names can be chosen by you!
+    Will work BUT NOT RECOMMENDED. 
+*/
+
+char sachin; 
+signed char katrina; 
+unsigned char sehwag;
+
+short int ramu; 
+unsigned short int moti; 
+
+int xyz; 
+unsigned int pqrpqrpqr; 
+
+long int a_b_c_d_e; 
+unsigned long int joker; 
+
+long long int troll; 
+unsigned long long int _a12______11111; 
+
+int main(void)
+{
+    
+    return (0); 
+}
+
