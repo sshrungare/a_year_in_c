4a4196e19f4ae3359d67bc60fb6a4e1d9a380bc1
Optimus <swapnil.shrungare123@gmail.com>
2022-11-17 22:20:44 +0530

    Added first vs project

---
+++ class/20221117_SESSION_028/HELLO_VISUAL_STUDIO/HELLO.C
@@ -0,0 +1,15 @@
+/*
+@Author : Swapnil Shrungare
+@Date : 17 Nov 2022
+@Goal : Display on console
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+	printf("\ Shree Ganeshay namah \n");
+	exit(0);
+}
