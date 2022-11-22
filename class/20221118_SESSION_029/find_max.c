00f26625578464efe2aa69ae0b48c18625384660
Optimus <swapnil.shrungare123@gmail.com>
2022-11-22 20:39:13 +0530

    Added Main

---
+++ class/20221118_SESSION_029/find_max.c
@@ -0,0 +1,12 @@
+/*
+@Author : Swapnil Shrungare
+@Date   : 18 Nov 2022
+@Goal  : Find maximum of 2 given number
+*/
+
+int get_max(int n1 , int n2);
+
+int get_max(int n1 , int n2)
+{
+    return n1;
+}
