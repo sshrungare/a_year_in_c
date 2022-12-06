6ca8d81a256885b009fe6aab2ff5ca853ab2aebb
Optimus <swapnil.shrungare123@gmail.com>
2022-12-06 14:01:19 +0530

    Added Symbolic constant

---
+++ class/20221201_SESSION_035/SYMBOLIC_CONSTANT.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MY_NUMBER   300
+
+int main(void)
+{
+    printf("NUMBER = %d\n" , 500 );
+    printf("NUMBER = %d\n", MY_NUMBER);
+
+    return(0);
+}
---
+++ class/20221201_SESSION_035/length_convertor.c
@@ -0,0 +1,24 @@
+/*
+@Author : Swapnil Shrungare
+@Goal : Create A length convertor console Application
+@Date : 6th Dec 2022
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main(void)
+{
+    #define TRUE    1 
+    #define FALSE   0
+
+    #define SUCCESS 1
+    #define FAILURE 0
+
+    #define MILIMETER   1
+    #define CENTIMETER  2
+    #define 
+
+    exit(0);
+}
