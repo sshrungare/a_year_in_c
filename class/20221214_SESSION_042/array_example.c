2e4ec6c010ba546c1cf820d97ba3ca55eba87419
Optimus <swapnil.shrungare123@gmail.com>
2022-12-14 20:10:39 +0530

    added array example

---
+++ class/20221214_SESSION_042/array_example.c
@@ -0,0 +1,17 @@
+/*
+@Author : Swapnil Shrungare
+@Date : 14 Dec 2022
+@Goal : Array Example
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int mark[5] = {12,1,2,3,4};
+    for( int i = 0 ; i<=5;i++)
+    {
+        printf("mark[%d] is %d\n",i,mark[i]);
+    }
+}
