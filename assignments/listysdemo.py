22a23b51643ba009616086562c50d565124a2699
Optimus <swapnil.shrungare123@gmail.com>
2023-01-05 15:06:30 +0530

    Added malloc

---
+++ assignments/listysdemo.py
@@ -0,0 +1,7 @@
+ 
+
+ss = "swapnil"
+
+ss_bew = ['S' if x == 'w'  else x for x in ss  ]
+
+print(''.join(ss_bew))
---
+++ class/20230104_SESSION_052/malloc_demo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(void){
+    int *p ;
+    int m;
+    p = (int*)malloc( sizeof(int)) ; 
+    if (p ==0){
+        puts("Error in allocating integer");
+        exit(-1);
+    }
+    *p = 200;
+    m = *p ;
+    printf("m %d\n",m);
+
+    free(p);
+    p = 0 ;
+    return(0);
+}
+
