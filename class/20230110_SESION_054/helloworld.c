36869f205f77a0b5d3d2f779939a76694d29053a
Optimus <swapnil.shrungare123@gmail.com>
2023-01-10 21:18:14 +0530

    Added first window program

--- class/20230110_SESION_054/helloworld.c
+++ class/20230110_SESION_054/helloworld.c
@@ -36,5 +36,65 @@ int WINAPI WinMain(HINSTANCE HInstance , HINSTANCE hPrevInstance , LPSTR lpCmdLi
         ExitProcess(EXIT_FAILURE);
     }
 
+    hwnd = CreateWindowEx(WS_EX_APPWINDOW,
+                          szClassName,
+                          szTitleOfWindow,
+                          WS_OVERLAPPEDWINDOW,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          CW_USEDEFAULT,
+                          NULL,
+                          NULL,
+                          HInstance,
+                          NULL
+                          );
+    
+    if(hwnd == NULL){
+        MessageBox(NULL,TEXT("Failed to create window!"),
+                    TEXT("CreateWindowEX"), MB_ICONERROR | MB_TOPMOST );
+        ExitProcess(EXIT_FAILURE);
+    }
+
+    ShowWindow(hwnd,nShowCmd);
+    UpdateWindow(hwnd);
+
+    while(GetMessage(&msg,NULL,0,0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (msg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg , WPARAM wParam, LPARAM lParam)
+{
+    static int cxScreen, cyScreen;
+    static TCHAR szPrompt[] = TEXT("SWAPNIL SHRUNGARE - ROLL NUMBER 178");
+    int iLen;
+
+    HDC hdc = NULL;
+    PAINTSTRUCT ps;
+
+    switch(uMsg)
+    {
+        case WM_SIZE: 
+            cxScreen = LOWORD(lParam); 
+            cyScreen = HIWORD(lParam); 
+            return (0); 
+
+        case WM_PAINT: 
+            hdc = BeginPaint(hwnd, &ps); 
+            iLen = lstrlen(szPrompt);         
+            SetTextAlign(hdc, TA_CENTER); 
+            TextOut(hdc, cxScreen/2, cyScreen/2, szPrompt, iLen); 
+            EndPaint(hwnd, &ps); 
+            return (0); 
+
+        case WM_DESTROY: 
+            PostQuitMessage(EXIT_SUCCESS); 
+            return (0); 
+    }   
+    return (DefWindowProc(hwnd, uMsg, wParam, lParam)); 
 }
 
