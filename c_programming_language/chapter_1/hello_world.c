030d1fe595df925955faedce9b47caf3be4f72aa
Optimus <swapnil.shrungare123@gmail.com>
2023-01-13 22:32:59 +0530

    added comments for outputs

--- c_programming_language/chapter_1/hello_world.c
+++ c_programming_language/chapter_1/hello_world.c
@@ -5,10 +5,20 @@
 */
 
 //
-//#include <stdio.h>
+#include <stdio.h>
+
+/*
+\n = for new line 
+\t = for tab
+\b = for backspace 
+\\ = for \ itself
+*/ 
 
 main()
 {
-    printf("Hello World!\n");
+    printf("Hello World!\n"); // "Hello World!"
+    printf("there is tab samp\tle\n"); // "there is tab samp    le"
+    printf("there is backspace samp\ble\n");//"there is backspace samble"
+    printf("there is backlash itself sa\\mple\n");//"there is backlash itself sa\mple"
 }
 
